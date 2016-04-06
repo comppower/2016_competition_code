9a57fd1ebb4231dd54a5964f68d1e9425bc514fe
RoxbotixDriver <comppower15@gmail.com>
2016-04-06 19:23:34 -0400

    test commit 2

--- Roxbotix_2016_Code/src/org/usfirst/frc/team4361/robot/Robot.java
+++ Roxbotix_2016_Code/src/org/usfirst/frc/team4361/robot/Robot.java
@@ -170,7 +170,7 @@ public class Robot extends IterativeRobot {
 	 * using the dashboard. The sendable chooser code works with the Java SmartDashboard. If you prefer the LabVIEW
 	 * Dashboard, remove all of the chooser code and uncomment the getString line to get the auto name from the text box
 	 * below the Gyro
-	 *
+	 *t
 	 * You can add additional auto modes by adding additional comparisons to the switch structure below with additional strings.
 	 * If using the SendableChooser make sure to add them to the chooser code above as well.
 	 */
